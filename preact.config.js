c75d47fef7f5366311689378104281c9ffa9ae33
Vitaliy Stanyshevskyy <vstanyshevskyy@stelladot.com>
2018-05-28 12:41:43 +0300

    Remove redirects file

--- preact.config.js
+++ preact.config.js
@@ -2,7 +2,6 @@ import CopyWebpackPlugin from 'copy-webpack-plugin';
 
 export default config => {
   config.plugins.push(new CopyWebpackPlugin([
-    { context: `${__dirname}/assets`, from: `*.*` },
-    { context: `${__dirname}/assets`, from: `_redirects` }
+    { context: `${__dirname}/assets`, from: `*.*` }
   ]));
 };
