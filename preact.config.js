eb6fe2ab49866572648a0ee72c62235d5c9dac04
Vitaliy Stanyshevskyy <vstanyshevskyy@stelladot.com>
2018-05-28 16:20:35 +0300

    Inline critical css

--- preact.config.js
+++ preact.config.js
@@ -1,7 +1,9 @@
 import CopyWebpackPlugin from 'copy-webpack-plugin';
+import CriticalCssPlugin from 'preact-cli-plugin-critical-css';
 
-export default config => {
+export default (config, env) => {
   config.plugins.push(new CopyWebpackPlugin([
     { context: `${__dirname}/assets`, from: 'icon.png', to: 'assets' }
   ]));
+  CriticalCssPlugin(config, env, {});
 };
